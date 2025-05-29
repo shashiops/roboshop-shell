a83c8bda75b3c478c4b8f0872b2cbe0a334161c7
Shashi <kodandashashidhar@gmail.com>
2025-05-30 02:15:56 +0530

    catalogue push

--- catalogue.sh
+++ catalogue.sh
@@ -12,7 +12,7 @@ echo "This script has started at $TIMESTAMP" &>> $LOG_FILE
 
 #validating after the excecution of every statement
 VALIDATE(){
-    if [ $1 -ne 0 ]then
+    if [ $1 -ne 0 ] then
         echo -e "Error:: $2 $RED Failed $NORMAL"
         exit 1
     else
@@ -29,13 +29,13 @@ else
 fi
 
 
-dnf module disable nodejs -y &>> LOG_FILE
+dnf module disable nodejs -y &>> $LOG_FILE
 VALIDATE $? "Disabling old module"
 
-dnf module enable nodejs:18 -y &>> LOG_FILE
+dnf module enable nodejs:18 -y &>> $LOG_FILE
 validating $? "Enabling Nodejs v18"
 
-dnf module disable nodejs - &>> LOG_FILE
+dnf module disable nodejs - &>> $LOG_FILE
 VALIDATE $? "Installing Nodejs"
 
 id roboshop
@@ -48,17 +48,37 @@ fi
 VALIDATE $? "Verifying user is created"
 
 
-mkdir -p /app &>> LOG_FILE
+mkdir -p /app &>> $LOG_FILE
 VALIDATE $? "Verifying if the dir is created"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> LOG_FILE
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOG_FILE
 VALIDATE $? "Downloading the Catalogue application"
 
 cd /app 
-unzip /tmp/catalogue.zip &>> LOG_FILE
+unzip /tmp/catalogue.zip &>> $LOG_FILE
 VALIDATE $? "Unzipping the file"
 
-npm install &>> LOG_FILE
-validating $? "NPM installing"
+npm install &>> $LOG_FILE
+VALIDATE $? "NPM installing"
 
 #create a catalogue service
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+VALIDATE $? "Copying the catalogue service file"
+
+systemctl daemon-reload &>> $LOG_FILE
+VALIDATE $? "catalogue daemon reload"
+
+systemctl enable catalogue &>> $LOG_FILE
+VALIDATE $? "Enable catalogue"
+
+systemctl start catalogue &>> $LOG_FILE
+VALIDATE $? "Starting catalogue"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org-shell -y &>> $LOG_FILE
+VALIDATE $? "Installing MongoDB client"
+
+mongo --host 172.31.86.37 </app/schema/catalogue.js &>> $LOG_FILE
+VALIDATE $? "Loading catalouge data into MongoDB"
